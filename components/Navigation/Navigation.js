8347de1a0c7b095a72c8a9754bccf6e4ebb62bf6
nap3831 <nap3831@gmail.com>
2020-04-16 02:57:15 +0700

    Created styles navbar scss

--- components/Navigation/Navigation.js
+++ components/Navigation/Navigation.js
@@ -1,6 +1,6 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState} from 'react';
 import Link from 'next/link';
-import '../navStyles.scss';
+import './navStyles.scss';
 
 const Navigation = (props) => {
     const [isToggleNav, setToggleNav] = useState(false);
