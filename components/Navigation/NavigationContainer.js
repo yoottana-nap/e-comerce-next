df03c446128fd736257228d721331496581c8eea
nap3831 <nap3831@gmail.com>
2020-04-18 03:26:32 +0700

    Cteated components component logic

--- components/Navigation/NavigationContainer.js
+++ components/Navigation/NavigationContainer.js
@@ -1,31 +1,31 @@
 import { connect } from 'react-redux';
 import Navigation from './Navigation';
 
-const navigationAction = {
-    TOGGLE_CLICK_NAVBAR: "TOGGLE_CLICK_NAVBAR"
-}
+// // Pattern Dispatch
+// const navigationAction = {
+//     TOGGLE_CLICK_NAVBAR: "TOGGLE_CLICK_NAVBAR"
+// }
+
 const navigationItems = [
     { keyTitle: "home", routePath: "/index" },
-    { keyTitle: "category", routePath: "/incorporation" },
+    { keyTitle: "category", routePath: "" },
     { keyTitle: "signup", routePath: "/signin" },
     { keyTitle: "singin", routePath: "/signin" },
     { keyTitle: "cart", routePath: "/signin" },
     { keyTitle: "order", routePath: "/signin" }
 ];
 
-let toggleNavbar = false;
-
 export const initialState = {
     navigationItems: navigationItems,
-    toggleNavbar: toggleNavbar
 };
 export const Navigationreducer = (state = initialState, action) => {
     switch (action.type) {
-        case navigationAction.TOGGLE_CLICK_NAVBAR:
-            return {
-                ...state,
-                toggleNavbar: action.payload
-            }
+        // // Pattern Dispatch
+        // case navigationAction.TOGGLE_CLICK_NAVBAR:
+        //     return {
+        //         ...state,
+        //         toggleNavbar: action.payload
+        //     }
     }
     return state;
 };
@@ -35,14 +35,15 @@ const mapStateToProps = (state) => {
         data: state
     }
 }
-const mapDispatchToProps = () => {
-    return ({
-        toggleNav: (data) => {
-            return {
-                type:navigationAction.TOGGLE_CLICK_NAVBAR,
-                payload:data
-            }
-        }
-    })
-}
-export default connect(mapStateToProps, mapDispatchToProps)(Navigation);
+// Pattern Dispatch
+// const mapDispatchToProps = () => {
+//     return ({
+//         toggleNav: (data) => {
+//             return {
+//                 type: navigationAction.TOGGLE_CLICK_NAVBAR,
+//                 payload: data
+//             }
+//         }
+//     })
+// }
+export default connect(mapStateToProps)(Navigation);
