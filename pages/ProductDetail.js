587fc92dd3c30fa4d1a15bc808b8b2599c9e3e15
nap3831 <nap3831@gmail.com>
2020-04-25 01:34:59 +0700

    Subscribe items form api to cartReducer

--- pages/ProductDetail.js
+++ pages/ProductDetail.js
@@ -1,13 +1,24 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import MainLayout from '../components/MainLayout/MainLayout';
 import ShowDetail from '../components/ProductDetailComponent/ShowDetail';
+import { mapDispatchToProps } from '../components/ProductDetailComponent/AddToCartContainer';
+import { connect } from 'react-redux';
 const ProductDetail = (props) => {
-    let { resId, resType, resData } = props;
+    let listItem = [];
+    let { resId, resType, resData,fetchingApi } = props;
+    useEffect(() => {
+        resData.forEach(dataArr => {
+           return dataArr.items.forEach(item => {
+               listItem.push(item)
+               fetchingApi(listItem);
+            })
+        });
+    },[]);
     return (
         <MainLayout>
             {resData.map(data => {
-               return data.type === resType && data.items.map((item, index) => {
-                    return parseInt(resId) === item.id && <ShowDetail resType={resType} key={index} item={item}/>
+                return data.type === resType && data.items.map((item, index) => {
+                    return parseInt(resId) === item.id && <ShowDetail resType={resType} key={index} item={item} />
                 })
             })}
         </MainLayout>
@@ -20,4 +31,4 @@ ProductDetail.getInitialProps = async ({ query }) => {
     return { resData: res, resId: query.id, resType: query.type }
 }
 
-export default ProductDetail;
+export default connect(state => state.cartreducer, mapDispatchToProps)(ProductDetail);
